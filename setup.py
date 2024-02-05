52a1ac621e54c8ea256ed2deca3134c257804740
CodeAnt AI <chinmay@codeant.ai>
2024-02-05 13:28:23 +0000

    changed files

--- setup.py
+++ setup.py
@@ -8,6 +8,36 @@ with open('README.rst') as readme_file:
 
 
 def recursive_include(relative_dir):
+    """
+    This function recursively includes all the file paths within a specified directory.
+    
+    Parameters:
+    - relative_dir (str): The relative directory path to search for files.
+    
+    Returns:
+    - list: A list of file paths within the specified directory.
+    
+    Processing Logic:
+    - The function first finds the root directory of the current file using the `os.path.dirname` and `os.path.abspath` functions.
+    - It then joins the root directory with the specified relative directory path to get the full path using `os.path.join`.
+    - Next, it uses the `os.walk` function to recursively iterate over all subdirectories and files within the full path.
+    - For each file found, it joins the root directory and the relative path of the file to create the absolute file path.
+    - The absolute file path is then appended to the `all_paths` list after removing the root directory path.
+    - Finally, the function returns the `all_paths` list containing all the file paths within the specified directory.
+    
+    Examples:
+    - Example usage of the function:
+    
+        ```
+        result = recursive_include('folder/subfolder')
+        print(result)  # Output: ['file1.txt', 'file2.txt', 'subfolder/file3.txt']
+        ```
+    
+        In this example, the function is called with the relative directory path 'folder/subfolder'.
+        It recursively searches for all file paths within this directory and its subdirectories.
+        The resulting list of file paths ['file1.txt', 'file2.txt', 'subfolder/file3.txt'] is then printed.
+    """
+    
     all_paths = []
     root_prefix = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), 'chalice')
